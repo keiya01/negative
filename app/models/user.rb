eb98c737cf413d198b6fdb21cb178ed4550df404
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-04-23 07:45:53 +0900

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   validates :username, presence: true
 
   def self.find_or_create_from_auth_hash(auth_hash)
-   provider = auth_hash[:provider]　#providerはどのサービスで認証したのかを見分けるもの
+   provider = auth_hash[:provider]
    uid = auth_hash[:uid]
    name = auth_hash[:info][:name]
    image_url = auth_hash[:info][:image]
 
-   #find_or_create_by()は()の中の条件のものが見つければ取得し、なければ新しく作成するというメソッド
    self.find_or_create_by(provider: provider,uid: uid) do |user|
      user.username = name
    end
