78298458c0090a7a8f418f048ba2d02fbb74edc6
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-04-24 07:26:39 +0900

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  mount_uploader :image_url, ImageUploader
+  mount_uploader :image, ImageUploader
   validates :provider, presence: true
   validates :uid, presence: true
   validates :username, presence: true
