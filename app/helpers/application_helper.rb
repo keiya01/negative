d0e85f959f00934b4da5cc373ed185f6618959a4
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-05-05 07:36:13 +0900

    edit twitter card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
       if page
         twitter_card[:url] = "https://www.locqer.com/users/#{page.id}"
         twitter_card[:title] = "問題を作ったよ！"
-        twitter_card[:description] = "問題に答えて隠し事を暴こう！"
+        twitter_card[:description] = "#{page.username}の問題に答えて隠し事を暴こう！"
         twitter_card[:image] = "#{asset_url("IMG_8323.JPG")}"
       else
         twitter_card[:url] = 'https://www.locqer.com'
@@ -19,7 +19,7 @@ module ApplicationHelper
         twitter_card[:description] = 'あなたの友達の問題を解いて秘密を打ち明けてみよう！'
         twitter_card[:image] = "#{asset_url("IMG_8323.JPG")}"
       end
-      twitter_card[:card] = 'summary'
+      twitter_card[:card] = 'summary_large_image'
       twitter_card[:site] = '@Yapy_service'
       twitter_card
   	end
