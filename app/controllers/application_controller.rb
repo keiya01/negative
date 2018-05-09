5755e355419c98795ee7c4b7638a0d2293cfd02a
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-05-09 20:17:51 +0900

    edit post notice modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :check_current_user
+  before_action :check_user_post
 
   def check_current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def check_user_post
+    @user_post ||= Post.find_by(user_id: @current_user.id) if @current_user
   end
 
   def brock_not_current_user
